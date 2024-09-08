784c39eda222b84d8aac3d99500d2424493015dd
qnxy <qnxy1997@gmail.com>
2024-09-09 02:50:04 +0800

    ClientContext 修改

--- terminal-server/src/main/java/com/qnxy/terminal/client/ClientContext.java
+++ terminal-server/src/main/java/com/qnxy/terminal/client/ClientContext.java
@@ -3,6 +3,7 @@ package com.qnxy.terminal.client;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.time.Duration;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -11,9 +12,10 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Data
 @Accessors(chain = true)
 public class ClientContext {
-    
+
     private final AtomicBoolean isAuth;
     private Long terminalId;
     private String imei;
-    
+    private Duration maxWaitMoveOutGoodsTime;
+
 }
